737292add95a407804dc432dcd2e0f40c48f0c21
Pablo Silva <120851304+pablo-frk@users.noreply.github.com>
2024-11-11 23:06:13 -0300

    Update Tasks.jsx

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,46 +1,45 @@
-import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import {
+    Check,
+    CheckCheckIcon,
+    ChevronRightIcon,
+    TrashIcon,
+} from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
-const Tasks = ({tasks, onTaskClick, onDeleteClick}) => {
+const Tasks = ({ tasks, onTaskClick, onDeleteClick }) => {
+    const navigate = useNavigate();
 
-    const navigate = useNavigate()
-    
     function onSeeDetailsClick(task) {
-        const query = new URLSearchParams()
-        query.set('title', task.title)
-        query.set('description', task.description)
-        navigate(`/task?${query.toString()}`)
+        const query = new URLSearchParams();
+        query.set("title", task.title);
+        query.set("description", task.description);
+        navigate(`/task?${query.toString()}`);
     }
-
-
 
     return (
         <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
             {tasks.map((task) => (
                 <li className="flex gap-2" key={task.id}>
-
                     <button
-                        className={`bg-slate-400 text-white p-2 rounded-md w-full text-left' ${
+                        className={`bg-slate-400 text-white p-2 rounded-md flex items-center gap-2 w-full text-left' ${
                             task.isCompleted && "line-through"
                         }`}
                         onClick={() => onTaskClick(task.id)}
                     >
+                        {task.isCompleted && <Check />}
                         {task.title}
                     </button>
 
-                    <Button 
-                    onClick={() => onSeeDetailsClick(task)}
-                    >
+                    <Button onClick={() => onSeeDetailsClick(task)}>
                         <ChevronRightIcon />
                     </Button>
 
-                    <button 
+                    <button
                         className="bg-slate-400 p-2 rounded-md text-white"
                         onClick={() => onDeleteClick(task.id)}
-                        >
+                    >
                         <TrashIcon />
                     </button>
-
                 </li>
             ))}
         </ul>
